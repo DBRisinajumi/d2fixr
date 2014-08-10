039c2878ababed795db9f9d54ccb99d0c3d7ea34
Uldis Nelsons <uldisnelsons@gmail.com>
2014-08-10 22:22:16 +0300

    Izlaota migration kļūda

--- migrations/m140805_192900_alter_fixr.php
+++ migrations/m140805_192900_alter_fixr.php
@@ -12,8 +12,8 @@ class m140805_192900_alter_fixr extends CDbMigration
             ALTER TABLE `fixr_fiit_x_ref` DROP FOREIGN KEY `fixr_fiit_x_ref_ibfk_6`; 
             ALTER TABLE `fixr_fiit_x_ref` DROP FOREIGN KEY `fixr_fiit_x_ref_ibfk_2`; 
             ALTER TABLE `fixr_fiit_x_ref`   
-                CHANGE `fixr_position_fret_id` `fixr_position_fret_id` TINYINT(3) UNSIGNED NULL,
-                CHANGE `fixr_period_fret_id` `fixr_period_fret_id` TINYINT(3) UNSIGNED NULL;
+                CHANGE `fixr_fret_id` `fixr_position_fret_id` TINYINT(3) UNSIGNED NULL,
+                CHANGE `fixr_frep_id` `fixr_period_fret_id` TINYINT(3) UNSIGNED NULL;
             ALTER TABLE `fixr_fiit_x_ref` ADD FOREIGN KEY (`fixr_position_fret_id`) REFERENCES `fret_ref_type`(`fret_id`); 
             ALTER TABLE `fixr_fiit_x_ref` ADD FOREIGN KEY (`fixr_period_fret_id`) REFERENCES `fret_ref_type`(`fret_id`); 
             DROP TABLE `frep_ref_period`;
